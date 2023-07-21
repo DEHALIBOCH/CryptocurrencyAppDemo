bca58fd06b5bdce07d8d6e87c23981c7fb8dd7a8
Marat <almabekov.marat@gmail.com>
2023-07-22 01:59:55 +0600

    Фикс возвращаемого значения в методах репозитория

--- app/src/main/java/com/demoapp/cleanarchdemo/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/demoapp/cleanarchdemo/data/repository/CoinRepositoryImpl.kt
@@ -3,6 +3,8 @@ package com.demoapp.cleanarchdemo.data.repository
 import com.demoapp.cleanarchdemo.data.remote.CoinApi
 import com.demoapp.cleanarchdemo.data.remote.dto.CoinDetailDto
 import com.demoapp.cleanarchdemo.data.remote.dto.CoinDto
+import com.demoapp.cleanarchdemo.domain.model.Coin
+import com.demoapp.cleanarchdemo.domain.model.CoinDetail
 import com.demoapp.cleanarchdemo.domain.repository.CoinRepository
 import javax.inject.Inject
 
@@ -10,11 +12,11 @@ class CoinRepositoryImpl @Inject constructor(
     private val api: CoinApi
 ) : CoinRepository {
 
-    override suspend fun getCoins(): List<CoinDto> {
-        return api.getCoins()
+    override suspend fun getCoins(): List<Coin> {
+        return api.getCoins().map { it.toCoin() }
     }
 
-    override suspend fun getCoinById(coinId: String): CoinDetailDto {
-        return api.getCoinById(coinId)
+    override suspend fun getCoinById(coinId: String): CoinDetail {
+        return api.getCoinById(coinId).toCoinDetail()
     }
 }
--- app/src/main/java/com/demoapp/cleanarchdemo/domain/repository/CoinRepository.kt
+++ app/src/main/java/com/demoapp/cleanarchdemo/domain/repository/CoinRepository.kt
@@ -1,17 +1,17 @@
 package com.demoapp.cleanarchdemo.domain.repository
 
-import com.demoapp.cleanarchdemo.data.remote.dto.CoinDetailDto
-import com.demoapp.cleanarchdemo.data.remote.dto.CoinDto
+import com.demoapp.cleanarchdemo.domain.model.Coin
+import com.demoapp.cleanarchdemo.domain.model.CoinDetail
 
 interface CoinRepository {
 
     /**
      * Метод для получения листа крипто-монет содержащих основную информацию
      */
-    suspend fun getCoins(): List<CoinDto>
+    suspend fun getCoins(): List<Coin>
 
     /**
      * Метод для получения крипто-монеты содержащей подробную информацию, используя id данной монеты
      */
-    suspend fun getCoinById(coinId: String): CoinDetailDto
+    suspend fun getCoinById(coinId: String): CoinDetail
 }
