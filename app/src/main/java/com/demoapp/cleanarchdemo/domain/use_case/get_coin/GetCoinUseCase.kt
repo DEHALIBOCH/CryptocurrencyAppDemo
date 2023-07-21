915a2daa7cd9ac5cd644dec4d55387ddc41c42ad
Marat <almabekov.marat@gmail.com>
2023-07-22 02:01:35 +0600

    UseCase для получения крипто-монеты, с подробной информацией о ней. используя ее id

---
+++ app/src/main/java/com/demoapp/cleanarchdemo/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -0,0 +1,30 @@
+package com.demoapp.cleanarchdemo.domain.use_case.get_coin
+
+import com.demoapp.cleanarchdemo.common.Resource
+import com.demoapp.cleanarchdemo.domain.model.CoinDetail
+import com.demoapp.cleanarchdemo.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+/**
+ * UseCase для получения крипто-монеты, с подробной информацией о ней. используя ее id
+ */
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId)
+            emit(Resource.Success(coin))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "Unexpected error"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+        }
+    }
+}
