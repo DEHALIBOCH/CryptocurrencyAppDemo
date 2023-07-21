c1b27ae1e53f264718414c08bdcb8be8cc9c35ce
Marat <almabekov.marat@gmail.com>
2023-07-22 00:33:48 +0600

    Тестовый запуск

--- app/src/main/java/com/demoapp/cleanarchdemo/MainActivity.kt
+++ app/src/main/java/com/demoapp/cleanarchdemo/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import com.demoapp.cleanarchdemo.theme.CleanArchDemoTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
