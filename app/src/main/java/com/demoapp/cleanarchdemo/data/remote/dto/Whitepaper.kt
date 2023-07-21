595e756c09dc1b470326ffb2ab30543fd11f57c7
Marat <almabekov.marat@gmail.com>
2023-07-22 01:40:14 +0600

    Класс репрезентующий модель крипто-монеты, содержащий подробную информацию о ней, который мы получем когда запрашиваем определенную монету импользуя ее id

---
+++ app/src/main/java/com/demoapp/cleanarchdemo/data/remote/dto/Whitepaper.kt
@@ -0,0 +1,11 @@
+package com.demoapp.cleanarchdemo.data.remote.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Whitepaper(
+    @SerializedName("link")
+    val link: String,
+    @SerializedName("thumbnail")
+    val thumbnail: String
+)
